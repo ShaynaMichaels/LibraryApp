fd5aa12b68e9cfcc8ed9eb3e3cc08e24f66978fd
Shira Smith <34427783+shirasmith987@users.noreply.github.com>
2018-05-17 15:10:59 -0400

    added string test

--- src/Member.java
+++ src/Member.java
@@ -1,13 +1,14 @@
 import java.util.ArrayList;
 
-public class Member {
+public class Member{
+    String test;
     String name;
     String status;
     ArrayList<Book> [] BooksOut;
 
     public Member(String name){
         this.name=name;
-        this.status='Regular';
+        this.status="Regular";
     }
 
     public void CheckOut(Book book){
