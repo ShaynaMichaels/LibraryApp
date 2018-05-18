4edcbaedb1224b11ee60ac298be800063ef98cdf
mshere1 <miriam.shere@mail.yu.edu>
2018-05-18 16:59:20 -0400

    Main Edits

--- src/edu/yu/oop/Main.java
+++ src/edu/yu/oop/Main.java
@@ -21,14 +21,21 @@ public class Main extends Application {
 
     public static void main(String[] args) {
         launch(args);
-        Member member = new Member();
-        member.loadMember(5);
+        Member member1 = new Member("Joe", 10);
+        Member member2 = new Member("Jane", 4);
+        Member member3 = new Member("John", 0);
+        member1.loadMember(5);
+        member2.loadMember(8);
+        member3.loadMember(4);
         Book book1 = new Book();
         Book book2 = new Book();
 
-        member.CheckOut(book1);
-        member.Renew(book1);
-        member.Return(book1);
-        
+        member1.CheckOut(book1);
+        member2.CheckOut(book1);
+        member1.Renew(book1);
+        member3.CheckOut(book1);
+        member1.Return(book1);
+        member2.CheckOut(book1);
+        member3.setState(FriendOfLibraryState);
     }
 }
